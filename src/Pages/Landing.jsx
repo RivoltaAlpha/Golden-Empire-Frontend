5f5ef93d89d90547a2949ee56d7f992ffb050399
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-03 03:02:52 +0300

    landing page

---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { backgroundMusic } from "../Utiils/soundManager";
+
+
+const Landing = () => {
+    const navigate = (path) => {
+        window.location.href = path;
+    };
+
+    const navigateToGame = () => {
+        navigate('/game');
+    };
+
+    useEffect(() => {
+      const handleUserInteraction = () => {
+        if (!backgroundMusic.playing()) {
+          backgroundMusic.play();
+        }
+        // Remove event listener after first interaction
+        window.removeEventListener("click", handleUserInteraction);
+      };
+
+      // Add a click event listener to start music on user interaction
+      window.addEventListener("click", handleUserInteraction);
+
+      return () => {
+        backgroundMusic.stop(); // Stop music when component unmounts (optional)
+        window.removeEventListener("click", handleUserInteraction);
+      };
+    }, []);
+
+    return (
+        <div className="h-screen absolute w-full bg-cover bg-center" style={{ backgroundImage: "url('/images/loadingpage.jpg')" }}>
+            <div className="flex items-center justify-center h-full bg-black bg-opacity-50">
+                <div className='relative gap-10 flex flex-col items-center'>
+                    <img src="/images/GoldenEmpiregame.png" alt="title" className="w-96 mx-auto" />
+                <button
+                    onClick={navigateToGame}
+                    className="px-8 mx-auto py-4 bg-yellow-500 text-white text-xl font-bold rounded hover:bg-yellow-600 transition duration-300"
+                >
+                    Start Game
+                </button>
+                </div>
+                <div className="absolute bottom-0 w-full text-center text-white text-xl">
+                    <p>© 2025. All Rights Reserved.</p>
+                </div>            
+            </div>
+        </div>
+    );
+};
+
+export default Landing;
