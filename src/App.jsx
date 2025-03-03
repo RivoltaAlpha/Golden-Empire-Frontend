251a2ced8c5197b3d50b874b15ca7d7305f8abcc
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-03 03:02:13 +0300

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import './index.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Landing from './Pages/Landing';
+// import Game from './Pages/Game';
 import GameGrid from "./components/GameGrid";
 
 const App = () => {
   return (
     <>
-    <div className="absolute inset-0 bg-cover bg-center max-h-screen"  style={{
-        backgroundImage:"url('/images/bgimage.jpg')",
-        overflow: "hidden",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}>
-    </div>
-    <div className="relative z-10 p-10">    
-    <GameGrid />
-    </div>
+          <Router>
+              <div className="absolute inset-0 bg-cover bg-center max-h-screen"  style={{
+                backgroundImage:"url('/images/bgimage.jpg')",
+                overflow: "hidden",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}>
+                <Routes>
+                  <Route path="/" element={<Landing />} />
+                  <Route path="/game" element={<GameGrid />} />
+               </Routes>
+            </div>
+        </Router>
     </>
     
   );
