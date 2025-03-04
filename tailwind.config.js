5506c13ea6acff63c7cc3df93f541fe719b1005a
RivoltaAlpha <mwanikitiffany25@gmail.com>
2025-03-04 05:57:35 +0300

    Enhance Tailwind CSS configuration to support custom grid row spans

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridRow: {
+        "span-2": "span 2 / span 2",
+        "span-3": "span 3 / span 3",
+      },
+    },
   },
   plugins: [],
 };
